3f65fdf35cfce30f0cb040ca8bf7840357b1fade
Rasputin2 <mcdnjmcd@aol.com>
2024-02-18 09:55:12 -0600

    Contains Page Icons

--- Home.py
+++ Home.py
@@ -1,12 +1,6 @@
 import time
-
 import streamlit as st
 
-st.set_page_config(
-    page_title="Home",
-    page_icon=":house:",
-    layout="wide"
-)
 
 st.title("Just Trig!")
 st.subheader("Where Everyone Learns to :heart: Trigonometry.")
